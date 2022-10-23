73d16fe8074314f5a6e679b39171af040832b16a
afazahamed <servermail2002@gmail.com>
2022-10-24 05:14:35 +0530

    Last push routing update 4

--- src/App.js
+++ src/App.js
@@ -10,14 +10,12 @@ import Single from "./pages/Single"
 
 function App() {
   return (
-    // <BrowserRouter>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/resume" element={<Home />}></Route>
         <Route path="/resume/:slug" element={<Single />}></Route>
       </Routes>
-    </HashRouter>
-    // </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
