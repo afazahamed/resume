5895dc52f7699368daa2528b00d222197306f29f
afazahamed <servermail2002@gmail.com>
2022-10-24 04:44:52 +0530

    third push 8

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/resume" element={<Home />}></Route>
         <Route path="/:slug" element={<Single />}></Route>
       </Routes>
     </BrowserRouter>
